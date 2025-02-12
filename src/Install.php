62a52a4adcca6f0497b78579b10c3c785856cf4d
Tony <57773117@qq.com>
2025-02-13 04:44:34 +0800

    加入App目录下的Request.php和Response.php

--- src/Install.php
+++ src/Install.php
@@ -19,9 +19,11 @@ class Install
      * 作用：用于在安装时，将源文件复制到目标位置
      */
     protected static array $pathRelation = [
-        'start.php'   => 'start.php', // 根目录下的 start.php
-        'windows.php' => 'windows.php', // 根目录下的 windows.php
-        'windows.bat' => 'windows.bat', // 根目录下的 windows.bat
+        'start.php'        => 'start.php', // 根目录下的 start.php
+        'windows.php'      => 'windows.php', // 根目录下的 windows.php
+        'windows.bat'      => 'windows.bat', // 根目录下的 windows.bat
+        'App/Request.php'  => 'app/Request.php', // App 目录下的 Request.php
+        'App/Response.php' => 'app/Response.php', // App 目录下的 Response.php
     ];
 
     /**
