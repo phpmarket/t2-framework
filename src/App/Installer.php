014e7a52fbd8f25c0d68fd6f1c94fd1818c20da5
Tony <57773117@qq.com>
2025-02-16 04:44:56 +0800

    composer.json

--- src/App/Installer.php
+++ src/App/Installer.php
@@ -9,117 +9,101 @@ use function method_exists;
 class Installer
 {
     /**
+     * Install.
+     *
+     * @param mixed $event
+     *
      * @return void
      */
-    public static function install(): void
+    public static function install(mixed $event): void
     {
-        file_put_contents('install.log', "Installing T2 Framework...\n", FILE_APPEND);
+        static::findHelper();
+        $psr4 = static::getPsr4($event);
+        foreach ($psr4 as $namespace => $path) {
+            $pluginConst = "\\{$namespace}Install::IS_PLUGIN";
+            if (!defined($pluginConst)) {
+                continue;
+            }
+            $installFunction = "\\{$namespace}Install::install";
+            if (is_callable($installFunction)) {
+                $installFunction(true);
+            }
+        }
     }
 
     /**
+     * Update.
+     *
+     * @param mixed $event
+     *
      * @return void
      */
-    public static function uninstall(): void
+    public static function update(mixed $event): void
     {
-        file_put_contents('install.log', "Uninstalling T2 Framework...\n", FILE_APPEND);
+        static::findHelper();
+        $psr4 = static::getPsr4($event);
+        foreach ($psr4 as $namespace => $path) {
+            $pluginConst = "\\{$namespace}Install::IS_PLUGIN";
+            if (!defined($pluginConst)) {
+                continue;
+            }
+            $updateFunction = "\\{$namespace}Install::update";
+            if (is_callable($updateFunction)) {
+                $updateFunction();
+                continue;
+            }
+            $installFunction = "\\{$namespace}Install::install";
+            if (is_callable($installFunction)) {
+                $installFunction(false);
+            }
+        }
     }
 
-//    /**
-//     * Install.
-//     *
-//     * @param mixed $event
-//     *
-//     * @return void
-//     */
-//    public static function install(mixed $event): void
-//    {
-//        static::findHelper();
-//        $psr4 = static::getPsr4($event);
-//        foreach ($psr4 as $namespace => $path) {
-//            $pluginConst = "\\{$namespace}Install::IS_PLUGIN";
-//            if (!defined($pluginConst)) {
-//                continue;
-//            }
-//            $installFunction = "\\{$namespace}Install::install";
-//            if (is_callable($installFunction)) {
-//                $installFunction(true);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Update.
-//     *
-//     * @param mixed $event
-//     *
-//     * @return void
-//     */
-//    public static function update(mixed $event): void
-//    {
-//        static::findHelper();
-//        $psr4 = static::getPsr4($event);
-//        foreach ($psr4 as $namespace => $path) {
-//            $pluginConst = "\\{$namespace}Install::IS_PLUGIN";
-//            if (!defined($pluginConst)) {
-//                continue;
-//            }
-//            $updateFunction = "\\{$namespace}Install::update";
-//            if (is_callable($updateFunction)) {
-//                $updateFunction();
-//                continue;
-//            }
-//            $installFunction = "\\{$namespace}Install::install";
-//            if (is_callable($installFunction)) {
-//                $installFunction(false);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Uninstall.
-//     *
-//     * @param mixed $event
-//     *
-//     * @return void
-//     */
-//    public static function uninstall(mixed $event): void
-//    {
-//        static::findHelper();
-//        $psr4 = static::getPsr4($event);
-//        foreach ($psr4 as $namespace => $path) {
-//            $pluginConst = "\\{$namespace}Install::IS_PLUGIN";
-//            if (!defined($pluginConst)) {
-//                continue;
-//            }
-//            $uninstallFunction = "\\{$namespace}Install::uninstall";
-//            if (is_callable($uninstallFunction)) {
-//                $uninstallFunction();
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Get psr-4 info
-//     *
-//     * @param mixed $event
-//     *
-//     * @return array|mixed
-//     */
-//    protected static function getPsr4(mixed $event): mixed
-//    {
-//        $operation = $event->getOperation();
-//        $autoload = method_exists($operation, 'getPackage') ? $operation->getPackage()->getAutoload() : $operation->getTargetPackage()->getAutoload();
-//        return $autoload['psr-4'] ?? [];
-//    }
-//
-//    /**
-//     * FindHelper.
-//     *
-//     * @return void
-//     */
-//    protected static function findHelper(): void
-//    {
-//        // Installer.php in T2Engine
-//        require_once __DIR__ . '/helpers.php';
-//    }
+    /**
+     * Uninstall.
+     *
+     * @param mixed $event
+     *
+     * @return void
+     */
+    public static function uninstall(mixed $event): void
+    {
+        static::findHelper();
+        $psr4 = static::getPsr4($event);
+        foreach ($psr4 as $namespace => $path) {
+            $pluginConst = "\\{$namespace}Install::IS_PLUGIN";
+            if (!defined($pluginConst)) {
+                continue;
+            }
+            $uninstallFunction = "\\{$namespace}Install::uninstall";
+            if (is_callable($uninstallFunction)) {
+                $uninstallFunction();
+            }
+        }
+    }
+
+    /**
+     * Get psr-4 info
+     *
+     * @param mixed $event
+     *
+     * @return array|mixed
+     */
+    protected static function getPsr4(mixed $event): mixed
+    {
+        $operation = $event->getOperation();
+        $autoload = method_exists($operation, 'getPackage') ? $operation->getPackage()->getAutoload() : $operation->getTargetPackage()->getAutoload();
+        return $autoload['psr-4'] ?? [];
+    }
+
+    /**
+     * FindHelper.
+     *
+     * @return void
+     */
+    protected static function findHelper(): void
+    {
+        // Installer.php in T2Engine
+        require_once __DIR__ . '/helpers.php';
+    }
 }
